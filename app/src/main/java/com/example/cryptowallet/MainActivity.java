f9b69f6db02771a1ee7fbdf11722b83422d45f8a
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-12-09 11:45:27 +0530

    pages added

--- app/src/main/java/com/example/cryptowallet/MainActivity.java
+++ app/src/main/java/com/example/cryptowallet/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.cryptowallet;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BottomTab.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
