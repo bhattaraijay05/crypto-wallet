7c60d09aa8ce43a1eb9ef08b0e4d311c5f24a678
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-12-10 18:05:55 +0530

    auth added

--- app/src/main/java/com/example/cryptowallet/MainActivity.java
+++ app/src/main/java/com/example/cryptowallet/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cryptowallet.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,24 +17,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.button);
-        Button logbut = (Button) findViewById(R.id.login);
+//        setContentView(R.layout.activity_main);
+//        Button btn = (Button) findViewById(R.id.button);
+//        Button logbut = (Button) findViewById(R.id.login);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), BottomTab.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+//        logbut.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BottomTab.class);
-                startActivity(intent);
-            }
-        });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            // Start home activity
+            startActivity(new Intent(MainActivity.this, BottomTab.class));
+        } else {
+            // No user is signed in
+            // start login activity
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
 
-
-        logbut.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        // close splash activity
+        finish();
     }
 }
